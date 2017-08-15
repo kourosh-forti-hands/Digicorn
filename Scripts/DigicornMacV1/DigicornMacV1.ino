fc48a9646b7fd33d9f3b2feb977e1152fcf3a5d5
Nicholas Toeniskoetter <ntoenis@gmail.com>
2017-08-15 14:00:18 -0500

    No idea if this works lol
    Small improvements

--- Scripts/DigicornMacV1/DigicornMacV1.ino
+++ Scripts/DigicornMacV1/DigicornMacV1.ino
@@ -23,27 +23,28 @@ void loop() {
   
   DigiKeyboard.sendKeyStroke(0);                           // This is generally not necessary but with some older systems it seems to prevent missing the first character after a delay:
   DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);     // Spotlight 
-  DigiKeyboard.delay(800);                                 // I have heard its not consistent to set a delay less than 800 but you could try
+  DigiKeyboard.delay(2000);                                 // I have heard its not consistent to set a delay less than 800 but you could try
   DigiKeyboard.println("outlook");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // May not be needed
   DigiKeyboard.delay(2000);
   
   
   DigiKeyboard.sendKeyStroke(KEY_N, MOD_GUI_LEFT);    // Creating New Email
-  DigiKeyboard.delay(800);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.println("<Enter Email Here>");             // I send the email to our group email, but you can put anything here
-  DigiKeyboard.delay(800);
+  DigiKeyboard.delay(20000);
   DigiKeyboard.sendKeyStroke(KEY_TAB);
-  DigiKeyboard.delay(800);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_TAB);
-  DigiKeyboard.delay(800);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.println("I Love Unicorns");                // Subject line
-  DigiKeyboard.delay(800);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_TAB);
-  DigiKeyboard.delay(800);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.println("http://img.freepik.com/free-vector/unicorn-background-design_1324-78.jpg?size=338&ext=jpg"); // Body
-  DigiKeyboard.delay(800);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_GUI_LEFT);// FunFact: CTRL+Enter can send an email 
-  DigiKeyboard.delay(800);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);                  // Also sometimes it has a pop up so we hit enter just incase.
 
   digitalWrite(0, HIGH);                                  // Lights go on to tell me to run away 

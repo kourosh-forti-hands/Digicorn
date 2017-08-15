27f3b72d21f2a0d50d9c497eea80d161d6af294c
Nicholas Toeniskoetter <ntoenis@gmail.com>
2017-08-15 13:51:35 -0500

    MacScript Testing
    I don't own a Mac, but from using one in the past I am attempting to write a simular script to work wither their key combos and key sets. Not tested and has many delays for testing.

---
+++ Scripts/DigicornMacV1/DigicornMacV0.5.ino
@@ -0,0 +1,52 @@
+#include "DigiKeyboard.h"d
+// Not all keys appear to be defined in DigiKeyboard.h so I improvised out of laziness
+#define KEY_UP_ARROW 0x52 
+#define KEY_DOWN_ARROW 0x51
+#define KEY_LEFT_ARROW 0x50
+#define KEY_RIGHT_ARROW 0x4F 
+#define KEY_TAB         0x2B
+
+void setup() {
+  // Set Pins 0 and 1 as outputs.
+  // Some Digisparks have a built-in LED on pin 0, while some have it on
+  // pin 1. This way, this works will all Digisparks.
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+}
+
+
+void loop() {
+  
+  int i=0;                                                 // var i is used for counting
+  digitalWrite(0, LOW);                                    // Using the light as a notification to when the script is done so I can run away
+  digitalWrite(1, LOW);                                    // Turns light off on pin 0 & 1
+  
+  DigiKeyboard.sendKeyStroke(0);                           // This is generally not necessary but with some older systems it seems to prevent missing the first character after a delay:
+  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);     // Spotlight 
+  DigiKeyboard.delay(800);                                 // I have heard its not consistent to set a delay less than 800 but you could try
+  DigiKeyboard.println("outlook");
+  DigiKeyboard.delay(2000);
+  
+  
+  DigiKeyboard.sendKeyStroke(KEY_N, MOD_GUI_LEFT);    // Creating New Email
+  DigiKeyboard.delay(800);
+  DigiKeyboard.println("<Enter Email Here>");             // I send the email to our group email, but you can put anything here
+  DigiKeyboard.delay(800);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(800);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(800);
+  DigiKeyboard.println("I Love Unicorns");                // Subject line
+  DigiKeyboard.delay(800);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(800);
+  DigiKeyboard.println("http://img.freepik.com/free-vector/unicorn-background-design_1324-78.jpg?size=338&ext=jpg"); // Body
+  DigiKeyboard.delay(800);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_GUI_LEFT);// FunFact: CTRL+Enter can send an email 
+  DigiKeyboard.delay(800);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);                  // Also sometimes it has a pop up so we hit enter just incase.
+
+  digitalWrite(0, HIGH);                                  // Lights go on to tell me to run away 
+  digitalWrite(1, HIGH);
+  DigiKeyboard.delay(20000);                              // 20sec delay incase I dont get it unplugged but you could remove this and have it loop
+}
